cdc43fe697acab51be83144d0adfd2a13c32e7a3
Drahoslav Madar <drahoslav.madar@gmail.com>
2024-01-27 01:16:42 +0100

    login modal

--- client/src/components/auth/LoginButton.jsx
+++ client/src/components/auth/LoginButton.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
 import { useHistory, useLocation } from "react-router-dom";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
+
+import {
+  IconButton,
+  Typography,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  TextField,
+  DialogActions,
+  Button,
+} from "@mui/material";
 
 import { useAuth } from "../../auth/ProvideAuth";
 import { ROUTES } from "../../common/config";
@@ -10,31 +20,74 @@ const LoginButton = () => {
   let location = useLocation();
   let auth = useAuth();
 
-  const dummyUser = {
-    email: "drahoslav.madar@gmail.com",
-    password: "123456abc",
-  };
-
   let { from } = location.state || { from: { pathname: ROUTES.home } };
 
+  const [open, setOpen] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const handleLogin = () => {
+    const user = { email, password };
     auth.signin(() => {
       history.replace(from);
-    }, dummyUser);
+    }, user);
+    handleClose();
   };
 
   return (
     !auth.user && (
-      <IconButton
-        size="small"
-        color="inherit"
-        aria-label="menu"
-        onClick={() => handleLogin()}
-      >
-        <Typography variant="h8" style={{ marginRight: "20px" }}>
-          Login
-        </Typography>
-      </IconButton>
+      <>
+        <IconButton
+          size="small"
+          color="inherit"
+          aria-label="menu"
+          onClick={handleOpen}
+        >
+          <Typography variant="h8" style={{ marginRight: "20px" }}>
+            Login
+          </Typography>
+        </IconButton>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>Login</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="email"
+              label="Email Address"
+              type="email"
+              fullWidth
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              margin="dense"
+              id="password"
+              label="Password"
+              type="password"
+              fullWidth
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={handleLogin} color="primary">
+              Login
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </>
     )
   );
 };
